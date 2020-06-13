1ff81bbc4ed46192118bcdb055047e468802bd61
stupid-2020 <ACG@computer.org>
2020-06-13 16:04:27 +0000

    Fix formatting issue

--- scripts/generate_version.py
+++ scripts/generate_version.py
@@ -7,21 +7,20 @@ from time import strftime
 def getVersionInfo():
     now = strftime("%Y-%m-%d %H:%M:%S")
     # If we would like the short form, add '--short' option
-    #sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
-    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    # sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    sha = subprocess.check_output(["git", "rev-parse", "HEAD"])
     # For Python3, convert bytes to str
     if isinstance(sha, bytes):
         sha = sha.decode()
     sha = sha.strip()
-    version = {
-        "date": now,
-        "sha": sha
-    }
+    version = {"date": now, "sha": sha}
     json_string = json.dumps(version, indent=4)
-    return(json_string)
+    return json_string
+
 
 def showUsage():
-    print('Usage: python[3] %s output_file' % sys.argv[0])
+    print("Usage: python[3] %s output_file" % sys.argv[0])
+
 
 def writeFile(path, string):
     f = open(path, "w")
