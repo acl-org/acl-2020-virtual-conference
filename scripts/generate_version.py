edc1e742fc50e4ee64c7d3fee1a907aa88d9fe60
stupid-2020 <ACG@computer.org>
2020-06-13 20:30:35 +0000

    Update script with the format suggested by isort, black and pylint

--- scripts/generate_version.py
+++ scripts/generate_version.py
@@ -1,24 +1,24 @@
+from __future__ import print_function
+
 import json
 import subprocess
 import sys
 from time import strftime
 
 
-def getVersionInfo():
+def get_version_info():
     now = strftime("%Y-%m-%d %H:%M:%S")
     # If we would like the short form, add '--short' option
     # sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
     sha = subprocess.check_output(["git", "rev-parse", "HEAD"])
     # For Python3, convert bytes to str
-    if isinstance(sha, bytes):
-        sha = sha.decode()
+    sha = sha.decode() if isinstance(sha, bytes) else sha
     sha = sha.strip()
     version = {"date": now, "sha": sha}
-    json_string = json.dumps(version, indent=4)
-    return json_string
+    return json.dumps(version, indent=4)
 
 
-def showUsage():
+def show_usage():
     print("Usage: python[3] %s output_file" % sys.argv[0])
 
 
@@ -31,9 +31,7 @@ def writeFile(path, string):
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         file_path = sys.argv[1]
+        json_string = get_version_info()
+        writeFile(file_path, json_string)
     else:
-        showUsage()
-        exit()
-
-    json_string = getVersionInfo()
-    writeFile(file_path, json_string)
+        show_usage()
