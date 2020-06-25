902e3bf6cfef125fd87548fc0c543d6c00038549
Robert <robert.alonso.ac@gmail.com>
2020-06-25 04:29:56 -0500

    Lint and update mypy.ini

--- chat/list_channels.py
+++ chat/list_channels.py
@@ -1,33 +1,52 @@
 import argparse
 import json
-import pprint
-from typing import Dict, List, Union, Any
+from typing import Any, Dict, List
 
-from tqdm import tqdm
-import yaml
 import pandas as pd
+import yaml
 from requests import sessions
 from rocketchat_API.rocketchat import RocketChat
+from tqdm import tqdm
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="MiniConf Portal Command Line")
-    parser.add_argument("--config", "-c", default="./config.yml", 
-                        help="Configuration yaml filepath. Default: ./config.yml")
-    parser.add_argument("--output_file", "-o", default="channels.csv", 
-                        help="Output csv filepath. Default: ./channels.csv")
-    parser.add_argument("--regexp", "--regex", "-r", default=None,
-                        help="Regular expression for filtering channels by name")
-    parser.add_argument("--filter_featured", '--featured', '-f', action="store_true",
-                        help="Output only featured channels")
+    parser.add_argument(
+        "--config",
+        "-c",
+        default="./config.yml",
+        help="Configuration yaml filepath. Default: ./config.yml",
+    )
+    parser.add_argument(
+        "--output_file",
+        "-o",
+        default="channels.csv",
+        help="Output csv filepath. Default: ./channels.csv",
+    )
+    parser.add_argument(
+        "--regexp",
+        "--regex",
+        "-r",
+        default=None,
+        help="Regular expression for filtering channels by name",
+    )
+    parser.add_argument(
+        "--filter_featured",
+        "--featured",
+        "-f",
+        action="store_true",
+        help="Output only featured channels",
+    )
     return parser.parse_args()
 
 
 def postprocess(channels: pd.DataFrame) -> pd.DataFrame:
     if "t" in channels:
         channel_types = {
-            "d": "Direct chat", "c": "Chat",
-            "p": "Private chat", "l": "Livechat"
+            "d": "Direct chat",
+            "c": "Chat",
+            "p": "Private chat",
+            "l": "Livechat",
         }
         channels.rename(columns={"t": "channelType"}, inplace=True)
         channels["channelType"] = channels["channelType"].map(channel_types)
@@ -41,8 +60,7 @@ def postprocess(channels: pd.DataFrame) -> pd.DataFrame:
     return channels
 
 
-def get_params(filter_featured: bool = False, 
-               regexp: str = None) -> Dict[str, str]:
+def get_params(filter_featured: bool = False, regexp: str = None) -> Dict[str, str]:
     # t: channel type (d: Direct chat, c: Chat, p: Private chat, l: Livechat)
     # msgs: number of messages
     fields = ["name", "msgs", "usersCount", "featured", "t", "topic", "_updatedAt"]
@@ -52,10 +70,10 @@ def get_params(filter_featured: bool = False,
         query["featured"] = True
     if regexp is not None:
         query["name"] = {"$regex": regexp}
-        
+
     params = {
         "fields": json.dumps({f: True for f in fields}),
-        "query": json.dumps(query)
+        "query": json.dumps(query),
     }
     return params
 
@@ -69,12 +87,12 @@ if __name__ == "__main__":
             user_id=config["user_id"],
             auth_token=config["auth_token"],
             server_url=config["server"],
-            session=session
+            session=session,
         )
-        params = get_params(args.filter_featured, args.regexp)
+        channels_params = get_params(args.filter_featured, args.regexp)
 
         channels_list: List[Dict] = []
-        channels_json = rocket.channels_list(**params).json()
+        channels_json = rocket.channels_list(**channels_params).json()
         channels_list.extend(channels_json["channels"])
 
         count, total = 50, channels_json["total"]
@@ -82,8 +100,9 @@ if __name__ == "__main__":
 
         for offset in tqdm(range(count, total, count)):
             channels_json = rocket.channels_list(
-                offset=offset, count=count, **params).json()
+                offset=offset, count=count, **channels_params
+            ).json()
             channels_list.extend(channels_json["channels"])
 
-        channels = postprocess(pd.DataFrame(channels_list))
-        channels.to_csv(args.output_file, index=False)
+        channels_df = postprocess(pd.DataFrame(channels_list))
+        channels_df.to_csv(args.output_file, index=False)
