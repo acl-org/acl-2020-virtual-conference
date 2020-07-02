dee922977b5ea0d276d6b855f5b2fb1936d22ff0
tlikhit <58948612+tlikhit@users.noreply.github.com>
2020-07-02 10:46:18 -0400

    Fix format

--- miniconf/load_site_data.py
+++ miniconf/load_site_data.py
@@ -516,7 +516,7 @@ def build_workshops(
                 material=item["material"],
                 livestream=item["livestream"],
                 papers=workshop_papers[item["UID"]],
-                schedule=workshop_schedules.get(item["UID"], "W1"),
+                schedule=workshop_schedules.get(item["UID"], workshop_schedules["W1"]),
                 zoom_link=item.get("zoom_link"),
                 session1_time=item.get("session1_time"),
                 session2_time=item.get("session2_time", ""),
