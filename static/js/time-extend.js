0d5ae0c3d492b915f2a44959411864543d50f015
Guy Aglionby <guy@guyaglionby.com>
2020-07-05 15:59:27 +0100

    Update script to display socials in local timezone

--- static/js/time-extend.js
+++ static/js/time-extend.js
@@ -1,23 +1,29 @@
 add_local_tz = (selector) => {
   const regex_time = new RegExp("\\((.*)-(.*) (.*)\\)");
   const guess_tz = moment.tz.guess(true);
+  const utc_offsets = { UTC: 0, GMT: 0, PDT: -7 };
 
   $(selector).each(function () {
     const t = $(this).text();
     const res = regex_time.exec(t);
-    if (res) {
-      const start_time = moment.utc(`2020-07-05 ${res[1]}`);
-      const end_time = moment.utc(`2020-07-05 ${res[2]}`);
-      const local_start = start_time.tz(guess_tz);
-      const local_start_and_tz = start_time.format("HH:mm");
-      const local_end = end_time.tz(guess_tz);
+    if (res && res[3] in utc_offsets) {
+      const utc_offset = utc_offsets[res[3]];
+      const start_time = moment
+        .utc(`2020-07-05 ${res[1]}`)
+        .utcOffset(utc_offset, true);
+      const end_time = moment
+        .utc(`2020-07-05 ${res[2]}`)
+        .utcOffset(utc_offset, true);
+      const local_start = moment(start_time).tz(guess_tz);
+      const local_start_and_tz = local_start.format("HH:mm");
+      const local_end = moment(end_time).tz(guess_tz);
       const local_end_and_tz = local_end.format("HH:mm z");
       let end_dd;
       if (start_time.isAfter(end_time)) {
         // needs to deal with "Jul 5 (22:00-01:30 GMT)", where the end time is actually +1d
-        end_dd = local_end.dayOfYear() - (end_time.utc().dayOfYear() - 1);
+        end_dd = local_end.dayOfYear() - (end_time.dayOfYear() - 1);
       } else {
-        end_dd = local_end.dayOfYear() - end_time.utc().dayOfYear();
+        end_dd = local_end.dayOfYear() - end_time.dayOfYear();
       }
       let end_dd_str = "";
       if (end_dd > 0) {
