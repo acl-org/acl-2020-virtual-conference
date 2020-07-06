409ed1c468d9941aec2c2c0ad976ca53c784c696
Hao Fang <haofang1990@gmail.com>
2020-07-06 16:59:05 -0700

    format

--- miniconf/load_site_data.py
+++ miniconf/load_site_data.py
@@ -581,10 +581,10 @@ def build_workshops(
                         session_name=session.get("name"),
                         start_time=parse_session_time(session.get("start_time")),
                         end_time=parse_session_time(session.get("end_time")),
-                        zoom_link=""
+                        zoom_link="",
                     )
                     for session in item.get("sessions")
-                ]
+                ],
             )
         )
     return workshops
